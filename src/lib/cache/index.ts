d037f99109895816de93b8aba9d878403013a84f
choi sung keun <86150470+cgoinglove@users.noreply.github.com>
2025-05-06 12:21:04 +0900

    feat: implement server cache initialization based on environment
    - Added conditional initialization of MemoryCache for serverCache based on IS_DEV.
    - Introduced global variable __server_cache__ to maintain cache state in development mode.

--- src/lib/cache/index.ts
+++ src/lib/cache/index.ts
@@ -1,3 +1,20 @@
+import { IS_DEV } from "lib/const";
 import { MemoryCache } from "./memory-cache";
 
-export const serverCache = new MemoryCache();
+let serverCache: MemoryCache;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __server__cache__: MemoryCache | undefined;
+}
+
+if (IS_DEV) {
+  if (!globalThis.__server_cache__) {
+    globalThis.__server_cache__ = new MemoryCache();
+  }
+  serverCache = globalThis.__server_cache__;
+} else {
+  serverCache = new MemoryCache();
+}
+
+export { serverCache };
