c1d3e3b1501ebdac2b2c7f169ddc33cf95f3d6f4
choi sung keun <86150470+cgoinglove@users.noreply.github.com>
2025-05-31 10:35:09 +0900

    feat: remove experimental caching option from Next.js config, add deepmerge dependency, and enhance message handling in i18n request for improved localization

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,16 +1,27 @@
 import { getRequestConfig } from "next-intl/server";
 import { safe } from "ts-safe";
 import { getLocaleAction } from "./get-locale";
+import deepmerge from "deepmerge";
+
+let defaultMessages: any = undefined;
 
 export default getRequestConfig(async () => {
   const locale = await getLocaleAction();
 
+  if (!defaultMessages) {
+    defaultMessages = (await import(`../../messages/en.json`)).default;
+  }
+
   const messages = await safe(() => import(`../../messages/${locale}.json`))
-    .ifFail(() => import(`../../messages/en.json`))
-    .unwrap();
+    .map((m) => m.default)
+    .orElse(defaultMessages);
 
   return {
     locale,
-    messages: messages.default,
+    messages:
+      locale === "en" ? defaultMessages : deepmerge(defaultMessages, messages),
+    getMessageFallback({ key, namespace }) {
+      return `${namespace}.${key}`;
+    },
   };
 });
