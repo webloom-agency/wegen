0a9bbc0944b8e1357a7f2beb88e03575fb053b19
choi sung keun <86150470+cgoinglove@users.noreply.github.com>
2025-06-02 20:00:54 +0900

    refactor: enhance locale retrieval logic
    - Added functions to validate locale and retrieve it from both cookies and headers.
    - Improved fallback mechanism to ensure a default locale is returned if none is found.

--- src/i18n/get-locale.ts
+++ src/i18n/get-locale.ts
@@ -1,9 +1,36 @@
 "use server";
-import { cookies } from "next/headers";
-import { COOKIE_KEY_LOCALE } from "lib/const";
+import { cookies, headers } from "next/headers";
+import { COOKIE_KEY_LOCALE, SUPPORTED_LOCALES } from "lib/const";
+
+function validateLocale(locale?: string): boolean {
+  return SUPPORTED_LOCALES.some((v) => v.code === locale);
+}
+
+async function getLocaleFromCookie(): Promise<string | undefined> {
+  const cookieStore = await cookies();
+  const locale = cookieStore.get(COOKIE_KEY_LOCALE)?.value;
+
+  return validateLocale(locale) ? locale : undefined;
+}
+
+async function getLocalFromHeader(): Promise<string | undefined> {
+  const headerStore = await headers();
+  const locale = headerStore
+    .get("accept-language")
+    ?.split(",")[0]
+    ?.trim()
+    .split("-")[0];
+
+  return validateLocale(locale) ? locale : undefined;
+}
 
 export async function getLocaleAction() {
-  const cookieStore = await cookies();
-  const locale = cookieStore.get(COOKIE_KEY_LOCALE)?.value || "en";
-  return locale;
+  let locale: string | undefined;
+
+  locale = await getLocaleFromCookie();
+  if (!locale) {
+    locale = await getLocalFromHeader();
+  }
+
+  return locale || SUPPORTED_LOCALES[0].code;
 }
